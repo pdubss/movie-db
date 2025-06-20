3227a56e7a51dca1d9067d726da853a58d18759a
Perry <perryy41996@yahoo.com>
2025-06-20 03:53:30 -0400

    setup layout and tanstack router

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,18 +3,20 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+    <div className="flex flex-col h-screen w-screen">
+      <nav className="p-2 h-14 bg-[rgb(17,17,17)] text-yellow-400">
+        <Link to="/">home</Link>
+        <Link to="/about">about me</Link>
+      </nav>
+      <div className="flex-1 bg-black p-2">
+        <main className="mx-auto w-2/3 h-full">
+          <Outlet />
+        </main>
       </div>
-      <hr />
-      <Outlet />
+      <footer className="p-2 bg-[rgb(17,17,17)] text-center text-white h-12">
+        Hello my footer
+      </footer>
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
 });
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,8 +6,15 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="p-4 h-full">
+      <div className="text-yellow-400 flex flex-col flex-1">
+        <h2 className=" text-2xl">Welcome to the Landing Page!</h2>
+        <p>
+          Here you'll be able to browse new movies to watch, and rate the ones
+          you've already finished!
+        </p>
+        <img src={"hello"} />
+      </div>
     </div>
   );
 }
