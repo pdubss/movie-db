ec7f744ce8fbeb5a8f18ae1a1a90a2a9f28c03d8
Perry <perryy41996@yahoo.com>
2025-07-11 01:24:03 -0400

    completed movieid/videos page

--- frontend/src/routes/movies/$movieId/videos.tsx
+++ frontend/src/routes/movies/$movieId/videos.tsx
@@ -1,9 +1,52 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Spinner from "@/components/ui/Spinner";
+import { getMovieById } from "@/queries/queries";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/movies/$movieId/videos')({
+export const Route = createFileRoute("/movies/$movieId/videos")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/movies/$movieId/videos"!</div>
+  const { movieId } = Route.useParams();
+  const { data, isLoading } = useQuery({
+    queryKey: ["details", movieId],
+    queryFn: () => getMovieById(movieId),
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="text-white">
+          <h2 className="my-6 text-center text-2xl">
+            Showing videos for{" "}
+            <Link
+              className="underline"
+              to="/movies/$movieId"
+              params={{ movieId }}
+            >
+              {data?.movie.title}
+            </Link>
+          </h2>
+          <ul className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+            {data &&
+              data.videos.length > 0 &&
+              data.videos.map((vid, index) => (
+                <li key={index} className="mb-4">
+                  <iframe
+                    src={`https://www.youtube.com/embed/${vid.key}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    loading="lazy"
+                    className="mx-auto aspect-[16/9] w-[80%]"
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
