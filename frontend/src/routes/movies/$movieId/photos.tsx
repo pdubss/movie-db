7d2a029f22babfc91089d4e46d6031fd89d4031f
Perry <perryy41996@yahoo.com>
2025-07-16 07:37:15 -0400

    implemented photos page with pagination for movieId

--- frontend/src/routes/movies/$movieId/photos.tsx
+++ frontend/src/routes/movies/$movieId/photos.tsx
@@ -1,9 +1,82 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Spinner from "@/components/ui/Spinner";
+import { getMovieById } from "@/queries/queries";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useState } from "react";
 
-export const Route = createFileRoute('/movies/$movieId/photos')({
+export const Route = createFileRoute("/movies/$movieId/photos")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/movies/$movieId/photos"!</div>
+  const { movieId } = Route.useParams();
+  const [page, setPage] = useState(1);
+  const { data, isLoading } = useQuery({
+    queryKey: ["details", movieId],
+    queryFn: () => getMovieById(movieId),
+  });
+  const BASE_URL = "https://image.tmdb.org/t/p/";
+
+  const start = (page - 1) * 20;
+  const end = page * 20;
+  const numPages = Math.ceil((data?.images.length ?? 0) / 20);
+
+  return (
+    <div className="h-full w-full">
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="flex h-full flex-1 flex-col items-center justify-center gap-8 text-white">
+          <h1 className="text-2xl">
+            Photos for{" "}
+            <Link
+              className="underline"
+              to="/movies/$movieId"
+              params={{ movieId }}
+            >
+              {data?.movie.title}
+            </Link>{" "}
+          </h1>
+          <ul className="grid grid-cols-5 gap-3">
+            {data?.images.slice(start, end).map((image) => (
+              <img
+                className="cursor-pointer rounded-md"
+                src={`${BASE_URL}w342${image.file_path}`}
+              />
+            ))}
+          </ul>
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex gap-4">
+              {" "}
+              <button
+                className="rounded-md bg-yellow-400 px-2 py-1 text-black"
+                disabled={page === 1}
+                onClick={() => setPage((page) => page - 1)}
+              >
+                Back
+              </button>
+              <button
+                className="rounded-md bg-yellow-400 px-2 py-1 text-black"
+                onClick={() => setPage((page) => page + 1)}
+              >
+                Next
+              </button>{" "}
+            </div>
+
+            <select className="w-14 bg-white text-center text-black">
+              {Array.from({ length: numPages }, (_, index) => (
+                <option
+                  className="bg-black text-center"
+                  key={index}
+                  value={page + 1}
+                >
+                  {index + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
